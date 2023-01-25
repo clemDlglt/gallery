e94da889aa2f8f351cc295abcc36367b198dc1ac
clemDlglt <clement.daligault@sfr.fr>
2023-01-25 14:40:57 +0100

    display folder icon

--- index.php
+++ index.php
@@ -1,13 +1,55 @@
 <?php
 $path = getcwd();
+$hidden_files = ['.git', 'index.php'];
+
 $scan = array_diff(scandir($path), array('.', '..'));
-var_dump($scan);
+$scan = array_diff($scan, $hidden_files);
 ?>
 
-<ul>
-    <?php
-    foreach($scan as $entry) {
-        echo '<li>' . $entry . '</li>';
-    }
-    ?>
-</ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gallery</title>
+
+    <style>
+        body {
+            font-family: sans-serif;
+        }
+
+        .list {
+            list-style-type: none;
+        }
+
+        .list-item a {
+            display: flex;
+            align-items: end;
+            gap: .5rem;
+            text-decoration: none;
+            font-weight: 500;
+            color: black;
+        }
+    </style>
+
+    <script defer>
+        
+    </script>
+</head>
+
+<body>
+    <ul class="list">
+        <?php foreach($scan as $entry) { ?>
+            <li class="list-item">
+                <a href="<?php echo $entry ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" style="width: 24px; height: 24px">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+                    </svg>
+                    <?php echo $entry ?>
+                </a>
+            </li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
